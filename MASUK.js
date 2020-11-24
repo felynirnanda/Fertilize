9ae54455b227cb4e61ab94c9047a9f82a7b6c7c3
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2020-11-24 17:36:54 +0700

    JS Masuk

---
+++ MASUK.js
@@ -0,0 +1,10 @@
+function login(){
+    var name = document.getElementById("nama").value;
+    var pass = document.getElementById("password").value;
+    if(name === '' || pass === ''){
+        alert("Harap isi semua bidangnya!");
+        return false;
+    }
+    alert("Selamat Datang " + name);
+    window.location.href="HOME.html";
+}
