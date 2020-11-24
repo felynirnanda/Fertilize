ab748eeac658fb5641038208e7a9edf9294587d4
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2020-11-24 15:55:30 +0700

    Javasript untuk DAFTAR.html

---
+++ DAFTAR.js
@@ -0,0 +1,13 @@
+function Daftar(){
+    var name = document.getElementById("nama").value;
+    var mail = document.getElementById("email").value;
+    var telp = document.getElementById("nomorTelp").value;
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+    if(name === '' || mail === '' || telp === '' || user === '' || pass === ''){
+        alert("Harap isi semua bidangnya!");
+        return false;
+    }
+    alert('Data Berhasil Diinputkan');
+    window.location.href='HOME.html';
+}
