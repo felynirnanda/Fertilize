621f56ac7d98ed60e86c769c94a4d48106ef878e
Wahid28 <71857145+Wahid28@users.noreply.github.com>
2020-11-25 18:22:46 +0700

    Home.js

---
+++ HOME.js
@@ -0,0 +1,71 @@
+function bestSeller(){
+    document.getElementById('href1').href='PESTISIDA INSEKTISIDA.html';
+    document.getElementById('href2').href='HERBISIDA RAMBO.html';
+    document.getElementById('href3').href='PESTISIDA INSEKTISIDA.html';
+    document.getElementById('href4').href='Bakterisida Nordox.html';
+
+    document.getElementById('gambar1').src='bahan/obat detail.jpeg';
+    document.getElementById('gambar2').src='bahan/obat detail 2.jpeg';
+    document.getElementById('gambar3').src='bahan/obat detail 3.jpeg';
+    document.getElementById('gambar4').src='bahan/obat detail 5.jpeg';
+    
+    document.getElementById('Nama1').innerHTML='Pestisida Insektisida 247 ZC 274C - 100mL';
+    document.getElementById('Nama2').innerHTML='Herbisida Rambo Gold 480 SL 1 Liter';
+    document.getElementById('Nama3').innerHTML='Insektisida Regent Red 50SC';
+    document.getElementById('Nama4').innerHTML='Fungisida / Bakterisida Nordox 56WP';
+
+    document.getElementById('Harga1').innerHTML='Rp 64.000';
+    document.getElementById('Harga2').innerHTML='Rp 60.000';
+    document.getElementById('Harga3').innerHTML='Rp 25.000';
+    document.getElementById('Harga4').innerHTML='Rp 25.000';
+
+    document.getElementsById('TulisanBawah').style.backgroundColor = 'blue';
+}
+
+function newArrival(){
+    document.getElementById('href1').href='index.html';
+    document.getElementById('href2').href='MASUK.html';
+    document.getElementById('href3').href='DAFTAR.html';
+    document.getElementById('href4').href='FUNGI MIKORIZA.html';
+
+    document.getElementById('gambar1').src='bahan/polybag.jpeg';
+    document.getElementById('gambar2').src='bahan/bibit jagung.jpeg';
+    document.getElementById('gambar3').src='bahan/obat detail 3.jpeg';
+    document.getElementById('gambar4').src='bahan/arit.jpeg';
+
+    document.getElementById('Nama1').innerHTML='POLYBAG';
+    document.getElementById('Nama2').innerHTML='Benih Pertanian Jagung BISI 2 - 1 Kg';
+    document.getElementById('Nama3').innerHTML='Insektisida Regent Red 50SC';
+    document.getElementById('Nama4').innerHTML='Arit Babatan Baja';
+    
+    document.getElementById('Harga1').innerHTML='Rp 3.000';
+    document.getElementById('Harga2').innerHTML='Rp 103.200';
+    document.getElementById('Harga3').innerHTML='Rp 25.000';
+    document.getElementById('Harga4').innerHTML='Rp 45.000';
+
+    document.body.style.backgroundColor = 'red';
+}
+
+function mostWanted(){
+    document.getElementById('href1').href='index.html';
+    document.getElementById('href2').href='MASUK.html';
+    document.getElementById('href3').href='DAFTAR.html';
+    document.getElementById('href4').href='FUNGI MIKORIZA.html';
+
+    document.getElementById('gambar1').src='bahan/arit.jpeg';
+    document.getElementById('gambar2').src='bahan/Garu.jpg';
+    document.getElementById('gambar3').src='bahan/bibit padi.jpeg';
+    document.getElementById('gambar4').src='bahan/bibit cabe.jpg';
+
+    document.getElementById('Nama1').innerHTML='Arit Babatan Baja';
+    document.getElementById('Nama2').innerHTML='Garu';
+    document.getElementById('Nama3').innerHTML='Benih Padi New Sertani 14';
+    document.getElementById('Nama4').innerHTML='Benih Cabe Keriting';
+
+    document.getElementById('Harga1').innerHTML='Rp 45.000';
+    document.getElementById('Harga2').innerHTML='Rp 95.000';
+    document.getElementById('Harga3').innerHTML='Rp 60.000';
+    document.getElementById('Harga4').innerHTML='Rp 20.000';
+
+    document.body.style.backgroundColor = 'white';
+}
